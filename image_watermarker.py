a56bb4756ad3580b65efa8b32df432c69621353c
Liam S. <liam.alan.smth@gmail.com>
2024-06-13 14:21:02 +0100

    Setup basic class layout for user_interface and display a window.

--- image_watermarker.py
+++ image_watermarker.py
@@ -1,3 +1,14 @@
+from user_interface import UI
+
+
 class ImageWatermarker:
     def __init__(self):
-        pass
+        self.ui = UI()
+
+    def run(self):
+        self.ui.start_event_loop()
+
+
+if __name__ == "__main__":
+    watermarker = ImageWatermarker()
+    watermarker.run()
--- user_interface.py
+++ user_interface.py
@@ -1,8 +1,30 @@
+import tkinter as tk
+
+
 class UI:
+    BACKGROUND_COLOUR = "grey"
+
     def __init__(self):
-        pass
+        self.window = tk.Tk()
+        self.window.title("Image Watermarker")
+        self.window.config(background=UI.BACKGROUND_COLOUR)
+
+        self.image = Image()
+        self.menu = Menu()
+
+    def start_event_loop(self):
+        self.window.mainloop()
 
 
 class Menu:
     def __init__(self):
-        pass
+        self.canvas = tk.Canvas(width=800, height=100, background="indigo")
+        self.canvas.grid(row=1, column=0)
+
+
+class Image:
+    def __init__(self):
+        self.canvas = tk.Canvas(width=700, height=326, background="black")
+        self.canvas.grid(row=0, column=0)
+
+
